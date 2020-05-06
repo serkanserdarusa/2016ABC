f0d768366c9b941f2d15f84fb8587a2bb2d57c9a
serkanserdarusa <serkanserdarusa@gmail.com>
2020-05-06 14:20:38 -0400

    commit

---
+++ src/main/java/JavaExample/Day_7_Abstraction.java
@@ -0,0 +1,52 @@
+package JavaExample;
+/*
+    it is a process of hiding implementation
+    Abstarct class allows us to create functionality and implement or override from subclasses
+    abstract class can contain regular method or abstract  method without body
+    if regular class extends  an abstract class,then abstract method meant to be overridden.(not regular method)
+    Access modifier should be same or more visible(in sub class)
+    we can not create object by using abstract class or interface
+    class-class==>extends
+    interface-interface==>extends
+    class-interface==>implement
+    class-class==>extends-inteface==>implement
+    abstract class cannot be private,final and static
+     */
+class father extends  Day_7_Abstraction{//father sub class Day_7_Abstraction super class
+    public  void salary(){
+        System.out.println("my salary is $5000");
+    }
+
+}
+class mother extends Day_7_Abstraction{
+//    public mother(){
+//        super();
+//        System.out.println("mother");
+//    }
+public  void salary(){
+    System.out.println("my salary is $5000");
+}
+
+
+}
+public  abstract class Day_7_Abstraction {//super class
+//    public Day_7_Abstraction(){
+//
+//        System.out.println("main");
+//    }
+    protected abstract void salary();
+    public void child(){
+        System.out.println("I am a child");
+    }
+
+    public static void main(String[] args) {
+        //Day_7_Abstraction obj=new Day_7_Abstraction();
+        father obj=new father();
+             obj.salary();
+             obj.child();
+        mother obj2=new mother();
+        obj2.salary();
+        obj2.child();
+    }
+
+}
---
+++ src/main/java/first/Day_7_protected_public.java
@@ -0,0 +1,17 @@
+package first;
+
+
+import JavaExample.Day_7_AccessModifier;
+
+public class Day_7_protected_public extends Day_7_AccessModifier {
+    public static void main(String[] args) {
+        Day_7_protected_public obj=new Day_7_protected_public();
+        System.out.println(obj.age);//protected
+        System.out.println(obj.name);//public
+        obj.A();//public
+        obj.B();//protected
+        //obj.C(0);//default
+
+
+    }
+}
