7f6773c25e0e60e71b379ba00da9328652b1c991
serkanserdarusa <serkanserdarusa@gmail.com>
2020-05-15 19:31:24 -0400

    commits

--- src/main/java/SQL/day1.java
+++ src/main/java/SQL/day1.java
@@ -8,11 +8,10 @@ public class day1 {
 --DDL=Data definition language
 --Create alter,alter table,drop,truncate
 
-create table ss (employee_id integer primary key,
+create table KLM (employee_id integer primary key,
 firstname varchar(20),
 lastname varchar(30),
 salary int);
-
 create table XYZ (employee_id integer primary key,
 firstname varchar(20),
 lastname varchar(30),
@@ -20,11 +19,19 @@ salary int);
 
 --insert into KLM(employee_id,firstname,lastname,salary) values (1,'mike','fan',4500);
 --insert into KLM values (1,'mike','fan',4500);
-
 insert all
 into ss values(1,'mike','fan',2500)
 into ss values(2,'scott','han',3500)
 into ss values(3,'john','kan',5500)
+into ss values(1,'mike','fan',2500)
+into ab values(2,'scott','han',3500)
+into ab values(3,'john','kan',5500)
+into KLM values(1,'mike','fan',2500)
+into KLM values(2,'scott','han',3500)
+into KLM values(3,'john','kan',5500)
+into XYZ values(1,'mike','fan',2500)
+into XYZ values(2,'scott','han',3500)
+into XYZ values(3,'john','kan',5500)
 select*from dual;
 
 --select
@@ -37,6 +44,7 @@ select*from klm,xyz;
 
 
 --update==>make some variable modified
+
 update KLM set firstname='Yunus' where employee_id=1;
 update XYZ set salary=10000 where employee_id=3;
 update XYZ set lastname='kilic' where rownum=1;
@@ -50,21 +58,16 @@ select*from klm;
 delete from KLM where employee_id =1;
 delete from KLM where firstname ='scott';
 select*from klm;
-
 --delete all row inside of the table but not table itself
 delete from klm;
 select*from XYZ;
-
 --truncate dosent accept where clause so we can not delete just one row
 truncate table XYZ;
 select*from XYZ;
-
 drop table klm;
 drop table xyz;
-select*from abcdd;
-drop table abcdd;
-select*from ab;
-
+select*from klm;
+drop table klm;
 --Alter==>modified
 --add,modify,drop,rename
 Alter table ab add adress varchar(30);
@@ -87,6 +90,7 @@ truncate table ss;
 select*from ss;
 rollback;
 select*from ss;
+
 
      */
 }
