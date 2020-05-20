6f3bd67abdbacc189d1e722ccf98a5345fa87c72
serkanserdarusa <serkanserdarusa@gmail.com>
2020-05-20 13:41:30 -0400

    commits

---
+++ src/main/java/Selenium/NavigateTo.java
@@ -0,0 +1,24 @@
+package Selenium;
+
+import Utils.BrowserUtils;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class NavigateTo {
+    public static void main(String[] args) {
+        System.setProperty("webdriver.chrome.driver","C:/Users/serka/eclipse-workspace/Batch15/chromedriver.exe");
+        WebDriver driver=new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("http://www.google.com");
+        BrowserUtils.wait(2);
+        System.out.println(driver.getTitle());
+        driver.get("http://www.amazon.com");
+        BrowserUtils.wait(2);
+        System.out.println(driver.getTitle());
+        driver.navigate().back();
+        BrowserUtils.wait(2);
+        driver.navigate().refresh();
+        BrowserUtils.wait(2);
+        driver.navigate().forward();
+    }
+}
